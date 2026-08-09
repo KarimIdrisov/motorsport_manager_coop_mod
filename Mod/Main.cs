0ca738fe51679b1099442a9dd5fa44d5ac605458
Karim Idrisov <karimidrisov@users.noreply.github.com>
2026-08-09 15:40:44 +1000

    Capture SaveSystem on all career load paths

--- Mod/Main.cs
+++ Mod/Main.cs
@@ -155,6 +155,9 @@ namespace MotorsportManagerCoop
             _harmony.Patch(
                 AccessTools.Method(typeof(SaveSystem), "LoadSaveWithName"),
                 prefix: new HarmonyMethod(typeof(Main), nameof(CaptureSaveSystem)));
+            _harmony.Patch(
+                AccessTools.Method(typeof(SaveSystem), "Load", new[] { typeof(SaveFileInfo), typeof(bool) }),
+                prefix: new HarmonyMethod(typeof(Main), nameof(CaptureSaveSystem)));
             _harmony.Patch(AccessTools.Method(typeof(Game), "OnLoad"),
                 postfix: new HarmonyMethod(typeof(Main), nameof(OnGameLoaded)));
             string autoRole = Environment.GetEnvironmentVariable("MM_COOP_AUTOSTART");
